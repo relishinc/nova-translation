3ce176eef2d897287ae0af579fc07d28bb318329
Steve Palmer <spalmer@users.noreply.github.com>
2023-10-16 16:30:27 -0700

    Update NovaTranslation.php

--- src/NovaTranslation.php
+++ src/NovaTranslation.php
@@ -2,11 +2,24 @@
 
 namespace Joedixon\NovaTranslation;
 
+use Illuminate\Http\Request;
 use Laravel\Nova\Nova;
 use Laravel\Nova\Tool;
 
 class NovaTranslation extends Tool
 {
+
+    /**
+    * Build the menu that renders the navigation links for the tool.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return mixed
+    */
+    public function menu(Request $request) 
+    {
+        return false;
+    }
+    
     /**
      * Perform any tasks that need to happen when the tool is booted.
      *
